b22f8328b007c5b8f24798d28cca728d18035716
dewin <dewinspena@gmail.com>
2021-11-23 22:46:09 -0500

    nextjs, typescript, material ui start

---
+++ frontend/src/theme.ts
@@ -0,0 +1,16 @@
+import { createTheme } from '@mui/material/styles';
+import { red } from '@mui/material/colors';
+
+// Create a theme instance.
+const theme = createTheme({
+  palette: {
+    primary: {
+      main: '#556cd6',
+    },
+    secondary: {
+      main: '#19857b',
+    },
+  },
+});
+
+export default theme;
